a7ba92301242e0a4bf8b1a66cdba1bf1e9ecfc89
Crendgrim <Crendgrim@users.noreply.github.com>
2022-05-23 18:50:14 +0200

    Undo/fix up the previous two commits
    This hopefully fixes odd crashes when MicroDurability is installed.

--- src/main/java/mod/crend/autohud/mixin/compat/microdurability/MicroDurabilityMixin.java
+++ src/main/java/mod/crend/autohud/mixin/compat/microdurability/MicroDurabilityMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = RendererBase.class, remap = false)
+@Mixin(value = RendererBase.class, priority = 1500)
 public class MicroDurabilityMixin {
     @ModifyArg(method = "onHudRender", at=@At(value = "INVOKE", target = "Lcom/github/reviversmc/microdurability/RendererBase;renderBar(Lnet/minecraft/item/ItemStack;II)V"), index = 2)
     private int autoHud$offsetRenderBar(int y) {

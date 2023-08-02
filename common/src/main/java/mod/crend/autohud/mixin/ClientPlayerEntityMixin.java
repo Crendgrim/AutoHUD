b595dc615b1d4bf5733277023a58306bc470e56d
CCr4ft3r <118105353+CCr4ft3r@users.noreply.github.com>
2023-08-02 19:29:34 +0200

    backport to 1.18.2

--- common/src/main/java/mod/crend/autohud/mixin/ClientPlayerEntityMixin.java
+++ common/src/main/java/mod/crend/autohud/mixin/ClientPlayerEntityMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.network.encryption.PlayerPublicKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,8 +19,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
 
-    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile, PlayerPublicKey playerPublicKey) {
-        super(world, profile, playerPublicKey);
+    public ClientPlayerEntityMixin(ClientWorld world, GameProfile profile) {
+        super(world, profile);
     }
 
     @Inject(method = "updateHealth", at = @At("TAIL"))
--- forge/src/main/java/mod/crend/autohud/forge/mixin/gui/VanillaGuiOverlayMixin.java
+++ forge/src/main/java/mod/crend/autohud/forge/mixin/gui/VanillaGuiOverlayMixin.java
@@ -7,113 +7,114 @@ import mod.crend.autohud.component.Component;
 import mod.crend.autohud.component.Hud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.scoreboard.ScoreboardObjective;
-import net.minecraftforge.client.gui.overlay.ForgeGui;
-import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.client.gui.ForgeIngameGui;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(VanillaGuiOverlay.class)
+@Mixin(ForgeIngameGui.class)
 @Debug(export = true)
 public class VanillaGuiOverlayMixin {
 
-	@WrapOperation(
-			method = "lambda$static$5(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderHotbar(FLnet/minecraft/client/util/math/MatrixStack;)V"
-			)
-	)
-	private static void autoHud$wrapHotbar(ForgeGui instance, float tickDelta, MatrixStack matrixStack, Operation<Void> original) {
-		if (AutoHud.targetHotbar) {
-			Hud.preInject(matrixStack, Component.Hotbar);
-		}
-		original.call(instance, tickDelta, matrixStack);
-		if (AutoHud.targetHotbar) {
-			Hud.postInject(matrixStack);
-		}
-	}
+    @WrapOperation(
+        method = "lambda$static$5(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderHotbar(FLnet/minecraft/client/util/math/MatrixStack;)V"
+        )
+    )
+    private static void autoHud$wrapHotbar(ForgeIngameGui instance, float tickDelta, MatrixStack matrixStack, Operation<Void> original) {
+        if (AutoHud.targetHotbar) {
+            Hud.preInject(matrixStack, Component.Hotbar);
+        }
+        original.call(instance, tickDelta, matrixStack);
+        if (AutoHud.targetHotbar) {
+            Hud.postInject(matrixStack);
+        }
+    }
 
-	@WrapOperation(
-			method = "lambda$static$8(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderHealth(IILnet/minecraft/client/util/math/MatrixStack;)V"
-			)
-	)
-	private static void autoHud$wrapHealth(ForgeGui instance, int width, int height, MatrixStack matrixStack, Operation<Void> original) {
-		if (AutoHud.targetStatusBars) {
-			Hud.preInject(matrixStack, Component.Health);
-		}
-		original.call(instance, width, height, matrixStack);
-		if (AutoHud.targetStatusBars) {
-			Hud.postInject(matrixStack);
-		}
-	}
-	@WrapOperation(
-			method = "lambda$static$9(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderArmor(Lnet/minecraft/client/util/math/MatrixStack;II)V"
-			)
-	)
-	private static void autoHud$wrapArmor(ForgeGui instance, MatrixStack matrixStack, int width, int height, Operation<Void> original) {
-		if (AutoHud.targetStatusBars) {
-			Hud.preInject(matrixStack, Component.Armor);
-		}
-		original.call(instance, matrixStack, width, height);
-		if (AutoHud.targetStatusBars) {
-			Hud.postInject(matrixStack);
-		}
-	}
-	@WrapOperation(
-			method = "lambda$static$10(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderFood(IILnet/minecraft/client/util/math/MatrixStack;)V"
-			)
-	)
-	private static void autoHud$wrapHunger(ForgeGui instance, int width, int height, MatrixStack matrixStack, Operation<Void> original) {
-		if (AutoHud.targetStatusBars) {
-			Hud.preInject(matrixStack, Component.Hunger);
-		}
-		original.call(instance, width, height, matrixStack);
-		if (AutoHud.targetStatusBars) {
-			Hud.postInject(matrixStack);
-		}
-	}
+    @WrapOperation(
+        method = "lambda$static$8(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderHealth(IILnet/minecraft/client/util/math/MatrixStack;)V"
+        )
+    )
+    private static void autoHud$wrapHealth(ForgeIngameGui instance, int width, int height, MatrixStack matrixStack, Operation<Void> original) {
+        if (AutoHud.targetStatusBars) {
+            Hud.preInject(matrixStack, Component.Health);
+        }
+        original.call(instance, width, height, matrixStack);
+        if (AutoHud.targetStatusBars) {
+            Hud.postInject(matrixStack);
+        }
+    }
 
-	@WrapOperation(
-			method = "lambda$static$15(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderHeldItemTooltip(Lnet/minecraft/client/util/math/MatrixStack;)V"
-			)
-	)
-	private static void autoHud$wrapTooltip(ForgeGui instance, MatrixStack matrixStack, Operation<Void> original) {
-		if (AutoHud.targetHotbar) {
-			Hud.preInject(matrixStack, Component.Tooltip);
-		}
-		original.call(instance, matrixStack);
-		if (AutoHud.targetStatusBars) {
-			Hud.postInject(matrixStack);
-		}
-	}
+    @WrapOperation(
+        method = "lambda$static$9(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderArmor(Lnet/minecraft/client/util/math/MatrixStack;II)V"
+        )
+    )
+    private static void autoHud$wrapArmor(ForgeIngameGui instance, MatrixStack matrixStack, int width, int height, Operation<Void> original) {
+        if (AutoHud.targetStatusBars) {
+            Hud.preInject(matrixStack, Component.Armor);
+        }
+        original.call(instance, matrixStack, width, height);
+        if (AutoHud.targetStatusBars) {
+            Hud.postInject(matrixStack);
+        }
+    }
 
-	@WrapOperation(
-			method = "lambda$static$23(Lnet/minecraftforge/client/gui/overlay/ForgeGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraftforge/client/gui/overlay/ForgeGui;renderScoreboardSidebar(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/scoreboard/ScoreboardObjective;)V"
-			)
-	)
-	private static void autoHud$wrapScoreboard(ForgeGui instance, MatrixStack matrixStack, ScoreboardObjective objective, Operation<Void> original) {
-		if (AutoHud.targetScoreboard) {
-			Hud.preInject(matrixStack, Component.Scoreboard);
-		}
-		original.call(instance, matrixStack, objective);
-		if (AutoHud.targetScoreboard) {
-			Hud.postInject(matrixStack);
-		}
-	}
+    @WrapOperation(
+        method = "lambda$static$10(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderFood(IILnet/minecraft/client/util/math/MatrixStack;)V"
+        )
+    )
+    private static void autoHud$wrapHunger(ForgeIngameGui instance, int width, int height, MatrixStack matrixStack, Operation<Void> original) {
+        if (AutoHud.targetStatusBars) {
+            Hud.preInject(matrixStack, Component.Hunger);
+        }
+        original.call(instance, width, height, matrixStack);
+        if (AutoHud.targetStatusBars) {
+            Hud.postInject(matrixStack);
+        }
+    }
+
+    @WrapOperation(
+        method = "lambda$static$15(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderHeldItemTooltip(Lnet/minecraft/client/util/math/MatrixStack;)V"
+        )
+    )
+    private static void autoHud$wrapTooltip(ForgeIngameGui instance, MatrixStack matrixStack, Operation<Void> original) {
+        if (AutoHud.targetHotbar) {
+            Hud.preInject(matrixStack, Component.Tooltip);
+        }
+        original.call(instance, matrixStack);
+        if (AutoHud.targetStatusBars) {
+            Hud.postInject(matrixStack);
+        }
+    }
+
+    @WrapOperation(
+        method = "lambda$static$23(Lnet/minecraftforge/client/gui/ForgeIngameGui;Lnet/minecraft/client/util/math/MatrixStack;FII)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;renderScoreboardSidebar(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/scoreboard/ScoreboardObjective;)V"
+        )
+    )
+    private static void autoHud$wrapScoreboard(ForgeIngameGui instance, MatrixStack matrixStack, ScoreboardObjective objective, Operation<Void> original) {
+        if (AutoHud.targetScoreboard) {
+            Hud.preInject(matrixStack, Component.Scoreboard);
+        }
+        original.call(instance, matrixStack, objective);
+        if (AutoHud.targetScoreboard) {
+            Hud.postInject(matrixStack);
+        }
+    }
 }

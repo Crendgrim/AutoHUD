793973283cda54c47785c5f6624706460a931c76
Crendgrim <Crendgrim@users.noreply.github.com>
2023-10-03 15:00:01 +0200

    MicroDurability does not need the magic anymore

--- fabric/src/main/java/mod/crend/autohud/compat/mixin/microdurability/MicroDurabilityMixin.java
+++ fabric/src/main/java/mod/crend/autohud/compat/mixin/microdurability/MicroDurabilityMixin.java
@@ -1,6 +1,6 @@
 package mod.crend.autohud.compat.mixin.microdurability;
 
-import com.github.reviversmc.microdurability.RendererBase;
+import com.github.reviversmc.microdurability.Renderer;
 import com.mojang.blaze3d.systems.RenderSystem;
 import mod.crend.autohud.compat.MicroDurabilityCompat;
 import mod.crend.autohud.render.AutoHudRenderer;
@@ -8,16 +8,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = RendererBase.class, priority = 1500)
+@Mixin(value = Renderer.class, priority = 1500)
 public class MicroDurabilityMixin {
-    @ModifyArg(method = "onHudRender", at=@At(value = "INVOKE", target = "Lcom/github/reviversmc/microdurability/RendererBase;renderBar(Lnet/minecraft/item/ItemStack;II)V"), index = 2)
+    @ModifyArg(method = "onHudRender", at=@At(value = "INVOKE", target = "Lcom/github/reviversmc/microdurability/Renderer;renderBar(Lnet/minecraft/item/ItemStack;II)V"), index = 2)
     private int autoHud$offsetRenderBar(int y) {
         return y + (int) MicroDurabilityCompat.MicroDurabilityComponent.getOffsetY(AutoHudRenderer.tickDelta);
     }
 
     @ModifyArg(
             method = "renderBar",
-            at=@At(value = "INVOKE", target = "Lcom/github/reviversmc/microdurability/RendererBase;renderGuiQuad(Lnet/minecraft/client/render/BufferBuilder;IIIIIIII)V"),
+            at=@At(value = "INVOKE", target = "Lcom/github/reviversmc/microdurability/Renderer;renderGuiQuad(Lnet/minecraft/client/render/BufferBuilder;IIIIIIII)V"),
             index=8
     )
     int autoHud$injectAlpha(int alpha) {

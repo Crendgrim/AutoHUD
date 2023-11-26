45ae4ab12d47ae35181beeddc39038c30b2330ab
Crendgrim <Crendgrim@users.noreply.github.com>
2023-11-26 20:09:07 +0100

    Greatly improve Dehydration compat

--- fabric/src/main/java/mod/crend/autohud/compat/mixin/dehydration/ThirstManagerMixin.java
+++ fabric/src/main/java/mod/crend/autohud/compat/mixin/dehydration/ThirstManagerMixin.java
@@ -2,6 +2,7 @@ package mod.crend.autohud.compat.mixin.dehydration;
 
 import mod.crend.autohud.compat.DehydrationCompat;
 import net.dehydration.thirst.ThirstManager;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ThirstManagerMixin {
     @Inject(method="setThirstLevel", at=@At("TAIL"))
     private void autoHud$setThirstLevel(int thirstLevel, CallbackInfo ci) {
+        DehydrationCompat.Thirst.updateState();
+    }
+
+    @Inject(method = "update", at=@At("TAIL"))
+    private void autoHud$update(PlayerEntity player, CallbackInfo ci) {
         DehydrationCompat.Thirst.updateState();
     }
 }

01daa655e6ef66feafbb86336bc99504db103754
Crendgrim <Crendgrim@users.noreply.github.com>
2023-12-02 13:29:48 +0100

    Bugfix: actually use the animation speed multipliers
    Somehow, they have been in the config forever, but were never used for
    speeding up animations.. oops

--- common/src/main/java/mod/crend/autohud/component/Component.java
+++ common/src/main/java/mod/crend/autohud/component/Component.java
@@ -286,8 +286,8 @@ public class Component {
     private void moveIn() {
         offset = Math.max(0, offset + offsetDelta);
         alpha = Math.min(1, alpha + alphaDelta);
-        double offsetSpeed = Math.sqrt(0.01 + offset) * 0.1 * AutoHud.config.animationSpeedMoveIn();
-        double alphaSpeed = 0.05 * AutoHud.config.animationSpeedFadeIn();
+        double offsetSpeed = Math.sqrt(0.01 + offset) * 0.1 * AutoHud.config.animationSpeedMoveIn() * config.speedMultiplier();
+        double alphaSpeed = 0.05 * AutoHud.config.animationSpeedFadeIn() * config.speedMultiplier();
         if (offset - offsetSpeed <= 0) {
             offsetDelta = -offset;
         } else {
@@ -302,8 +302,8 @@ public class Component {
     private void moveOut() {
         offset = Math.min(1, offset + offsetDelta);
         alpha = Math.max(0, alpha + alphaDelta);
-        double offsetSpeed = Math.sqrt(0.01 + offset) * 0.1 * AutoHud.config.animationSpeedMoveOut();
-        double alphaSpeed = 0.05 * AutoHud.config.animationSpeedFadeOut();
+        double offsetSpeed = Math.sqrt(0.01 + offset) * 0.1 * AutoHud.config.animationSpeedMoveOut() * config.speedMultiplier();
+        double alphaSpeed = 0.05 * AutoHud.config.animationSpeedFadeOut() * config.speedMultiplier();
         if (offset + offsetSpeed >= 1) {
             offsetDelta = 1 - offset;
         } else {

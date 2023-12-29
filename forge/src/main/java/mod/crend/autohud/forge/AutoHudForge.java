53f703fc97d824595010e6e5ade8bf3d69a6e22e
Crendgrim <Crendgrim@users.noreply.github.com>
2023-12-29 17:51:39 +0100

    Add Raised compatibility on Forge

--- forge/src/main/java/mod/crend/autohud/forge/AutoHudForge.java
+++ forge/src/main/java/mod/crend/autohud/forge/AutoHudForge.java
@@ -3,10 +3,12 @@ package mod.crend.autohud.forge;
 import mod.crend.autohud.AutoHud;
 import mod.crend.autohud.ModKeyBindings;
 import mod.crend.autohud.component.Hud;
+import mod.crend.autohud.forge.compat.RaisedCompat;
 import net.minecraft.client.MinecraftClient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(AutoHud.MOD_ID)
@@ -17,6 +19,10 @@ public class AutoHudForge {
         if (event.phase == TickEvent.Phase.END) {
             ModKeyBindings.clientTick(MinecraftClient.getInstance());
             Hud.tick();
+
+            if (ModList.get().isLoaded("raised")) {
+                RaisedCompat.tick();
+            }
         }
     }
 }
